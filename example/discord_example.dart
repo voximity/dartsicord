cc3405eba01e9e4e88fadd688a8a921697aa4914
vox <0zanfran@gmail.com>
2017-12-03 15:40:11 -0600

    Initial commit.

---
+++ example/discord_example.dart
@@ -0,0 +1,1 @@
+import "package:discord/discord.dart";
---
+++ test/discord_test.dart
@@ -0,0 +1,16 @@
+import 'package:discord/discord.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = new Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
