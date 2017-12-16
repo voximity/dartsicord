9be5a0f057c11be29fcb496e2e8429274db55dfd
vox <0zanfran@gmail.com>
2017-12-16 11:50:49 -0600

    Add exports to WS classes

--- lib/dartsicord.dart
+++ lib/dartsicord.dart
@@ -1,5 +1,11 @@
 library dartsicord;
 
+export "src/ws/channel.dart";
 export "src/ws/embed.dart";
+export "src/ws/guild.dart";
+export "src/ws/message.dart";
+export "src/ws/permission.dart";
+export "src/ws/user.dart";
+
 export "src/client.dart";
 export "src/enums.dart";
